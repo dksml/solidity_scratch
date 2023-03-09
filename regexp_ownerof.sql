8f0b423c21ec1aea9269b7e8252133224355f7a2
dksml <110994507+dksml@users.noreply.github.com>
2023-03-09 16:27:15 -0500

    Create regexp_ownerof.sql

---
+++ regexp_ownerof.sql
@@ -0,0 +1,16 @@
+select 
+-- `offset`,
+-- REGEXP_EXTRACT(log, 'function ownerOf.{52}', 0)
+-- REGEXP_EXTRACT(log, 'function ownerOf.{50}', 0)
+-- ,REGEXP_EXTRACT(log, 'function ownerOf', 0)\
+--  REGEXP_EXTRACT(log, 'function ownerOf.*\{.*\}function', 0)
+--  REGEXP_EXTRACT(log, 'function ownerOf.*\{.*\}function', 0)
+-- ,`offset`
+`offset`
+,REGEXP_EXTRACT(log, 'function[\s]*ownerOf(?:(?!function).)*?\{(?:(?!function).)*?\}', 0)
+-- ,REGEXP_EXTRACT(data[1]['SourceCode'], 'function ownerOf.*function', 0)
+from `hive`.`erc_balances`.`etherscan_verified_contracts_v0`
+where REGEXP(log, 'function[\s]*ownerOf(?:(?!function).)*?\{(?:(?!function).)*?\}') 
+and log like '%721%'
+limit 100;
+
